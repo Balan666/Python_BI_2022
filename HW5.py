71b2bc1f189ad0ea8f96a12e0fb52a2183bdea06
Balan <annabalannn267@gmail.com>
2022-11-10 21:43:56 +0300

    pandas: dataframes&graphics

--- HW5.py
+++ HW5.py
@@ -1,29 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from pandas import DataFrame
+import seaborn as sns
+import numpy as np
 
-
-def read_gff(x):
-    df = pd.read_csv(x, sep='\t', comment='t',
+def read_gff(file_path):
+    gff = pd.read_csv(file_path, sep='\t', comment='t',
                      names=['seqid', 'resourse', 'type', 'start', 'end', 'score', 'strand', 'frame', 'attribute'])
-    df.drop(index = df[df['seqid'].str.contains('#')].index, inplace=True)
-    df.reset_index(inplace=True)
-    df['attribute'] = df['attribute'].astype(str).apply(lambda x: x.split('_')[0].split('=')[1])
-    return df
+    gff.drop(index = gff[gff['seqid'].str.contains('#')].index, inplace=True)
+    gff.reset_index(inplace=True)
+    gff['attribute'] = gff['attribute'].astype(str).apply(lambda x: x.split('_')[0].split('=')[1])
+    return gff
 
 
-def read_bed6(x):
-    df = pd.read_csv(x, sep='\t',  names = ['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand'])
-    return df
+def read_bed6(file_path):
+    bed6 = pd.read_csv(file_path, sep='\t',  names = ['chrom', 'chromStart', 'chromEnd', 'name', 'score', 'strand'])
+    return bed6
 
 
-def rna_count(x):
-    df = read_gff(x)
-    df2 = df.groupby(['seqid','attribute']).size()
-    return df2.plot()
+def rna_count(file_path):
+    gff = read_gff(file_path)
+    rnas_count = gff.groupby(['seqid','attribute']).size().to_frame().reset_index()
+    rnas_count = rnas_count.rename(columns={0:'quantity', 'attribute':'RNA type'})
+    return rnas_count
 
 
-#print(read_gff('rrna_annotation.gff'))
-#print(read_bed6('alignment.bed'))
-#print(df.index(df[df['seqid'].str.contains("#")]).tolist())
-print(rna_count('rrna_annotation.gff'))
+gff = read_gff('/rrna_annotation.gff')
+bed = read_bed6('alignment.bed')
+gff.join(bed,lsuffix='_caller', rsuffix='_other')
+
+gff_file = rna_count('/rrna_annotation.gff')
+gff_file.pivot(index='seqid', columns='RNA type', values='quantity').plot.bar(figsize=(15,8))
